c15ba550ed203b09c503305d2136297e903a2cfa
vishalf15 <vishalf15@gmail.com>
2018-05-25 11:08:57 +0530

    Eureka config server tutorial
    Eureka config server and client have created.
    1. Config server is cable to establish connection with Git with encrypted password.
    2. Dynamic reloading is also possible

---
+++ eureka-client/src/main/java/com/sclient/eureka/util/ClientConstants.java
@@ -0,0 +1,53 @@
+package com.sclient.eureka.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class ClientConstants {
+	@Value("${first.name}")
+	private String name;
+
+	@Value("${user.role}")
+	private String role;
+	
+	@Value("${user.pass}")
+	private String pass;
+	
+	@Value("${user.pass2}")
+	private String pass2;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getPass2() {
+		return pass2;
+	}
+
+	public void setPass2(String pass2) {
+		this.pass2 = pass2;
+	}
+}
